423e42bbe350edb14a52febeae7b40689af83989
ajhudson <andyhudson1979@gmail.com>
2020-02-18 22:26:02 +0000

    Added server and dummy test data

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = function() {
+    return {
+        categories: ['Watersports', 'Soccer', 'Chess'],
+        products: [
+            { id: 1, name: 'Kayak', category: 'Watersports', description: 'A boat for one person', price: 275 },
+            { id: 2, name: 'Lifejacket', category: 'Watersports', description: 'Protective and fashionable', price: 48.95 },
+            { id: 3, name: 'Soccer Ball', category: 'Soccer', description: 'FIFA approved', price: 19.50 },
+            { id: 4, name: 'Corner Flags', category: 'Soccer', description: 'Things on the corners', price: 34.95 },
+            { id: 5, name: 'Stadium', category: 'Soccer', description: '25,000 all seater', price: 79500 },
+            { id: 6, name: 'Thinking Cap', category: 'Chess', description: 'Improve your brain', price: 16 },
+            { id: 7, name: 'Unsteady Chair', category: 'Chess', description: 'Put your opponent off', price: 29.95 },
+            { id: 8, name: 'Human Chess Board', category: 'Chess', description: 'Fun family game', price: 75 },
+            { id: 9, name: 'Bling bling king', category: 'Chess', description: 'Gold plated thingy', price: 1200 }
+        ],
+        orders: []
+    }
+}
