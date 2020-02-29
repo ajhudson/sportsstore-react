dca1d324987e71173a07ba1f3c8554445f59a459
ajhudson <andyhudson1979@gmail.com>
2020-02-29 10:15:19 +0000

    check in

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,10 +1,10 @@
 import { ActionTypes} from "./Types";
-import { data as phData} from "./placeholderData";
+//import { data as phData} from "./placeholderData";
+import { RestDataSource } from './RestDatasource';
+
+const datasource = new RestDataSource();
 
 export const loadData = (dataType) => ({
     type: ActionTypes.DATA_LOAD,
-    payload: {
-        dataType: dataType,
-        data: phData[dataType]
-    }
+    payload: datasource.GetData(dataType).then(response => ({ dataType, data: response.data}))
 });
--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,7 +1,7 @@
-import { createStore } from "redux";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
+import { createStore, applyMiddleware } from 'redux';
+import { asyncActions } from './AsyncMiddleware';
 
-export const SportsStoreDataStore 
-    = createStore(CommonReducer(ShopReducer, CartReducer));
+export const SportsStoreDataStore  = createStore(CommonReducer(ShopReducer, CartReducer), applyMiddleware(asyncActions));
